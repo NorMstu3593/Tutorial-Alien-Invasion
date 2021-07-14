9b4d6876131f93c9d9ae0b739f9784b21d903349
flxd <xcbflxd@163.com>
2021-07-14 11:27:31 +0800

    fix bugs, 添加子弹类

--- settings.py
+++ settings.py
@@ -10,3 +10,9 @@ class Settings:
 
         # 飞船设置
         self.ship_speed = 1.5
+
+        # 子弹设置（宽3像素，高15像素，深灰色）
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
